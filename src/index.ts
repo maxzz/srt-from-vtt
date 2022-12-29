128ae40005529ec39db46a24a23575bbc1eb823d
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-29 15:22:38 -0800

    WIP. add app skeleton. main

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import chalk from 'chalk';
+import { exitProcess, newErrorArgs } from './utils/utils-errors';
+import { Targets } from './app/app-types';
+import { getTargets } from './app/app-arguments';
+import { help } from './app/app-help';
+import { notes } from './app/app-notes';
+import { handleFiles, handleFolders } from './app/app';
+
+async function main() {
+    const targets: Targets = getTargets();
+
+    if (targets.files.length) {
+        handleFiles([...targets.files, ...targets.dirs]);
+    } else if (targets.dirs.length) {
+        handleFolders(targets.dirs);
+    } else {
+        throw newErrorArgs(`Specify at leats one folder or files name to process`);
+    }
+
+    if (notes.willShow()) {
+        if (targets.dirs.length) {
+            let rootDir = path.dirname(targets.dirs[0]);
+            console.log(chalk.blueBright(`Processed root:\n${rootDir}`));
+        }
+        //TODO: else [...targets.files, ...targets.dirs]
+    }
+
+    await notes.show(false);
+}
+
+main().catch(async (error) => {
+    error.args && help(); // Show help only if arguments are invalid.
+    let errorMsg = `${notes.buildMessage()}${chalk[error.args ? 'yellow' : 'red'](`\n${error.message}`)}`;
+    await exitProcess(1, errorMsg);
+});
