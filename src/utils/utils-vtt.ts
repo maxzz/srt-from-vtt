3f0de78c65d0f02ceaab5c2ef4d48a7037df1f04
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-30 14:30:26 -0800

    WIP. utils-vtt. switch back to non es6 version (var vs. let errors)

--- src/utils/utils-vtt.ts
+++ src/utils/utils-vtt.ts
@@ -1,9 +1,89 @@
-import { EOL } from 'os';
+'use strict';
+
+var _through = require('through2');
+var _through2 = _interopRequireDefault(_through);
+var _split = require('split2');
+var _split2 = _interopRequireDefault(_split);
+var _pumpify = require('pumpify');
+var _pumpify2 = _interopRequireDefault(_pumpify);
+
+var EOL = require('os').EOL;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function () {
 
+  var count = 0;
+  var reg = new RegExp('(WEBVTT\s*(FILE)?.*)('+EOL+')*', 'g' )
+  var write = function write(line, enc, cb) {
+
+    if (!line.trim()) return cb();
+    
+    if(line.match(/(\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2})\.(\d{3}\s*)/g))
+    {
+      var vttComp = line.split('-->')
+      var vttLine = vttComp.map(function(item){
+        item = item.replace('.',',');
+        if(item.split(":").length < 3)
+        {
+          item = '00:'+item.trim(); 
+        }  
+        return item;
+      }).join(' --> ');
+      vttLine = vttLine+EOL
+    }
+    else if(line.match(/(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)/g))
+    {
+      var vttComp = line.split('-->')
+      var vttLine = vttComp.map(function(item){
+        item = item.replace('.',',');
+        if(item.split(":").length < 3)
+        {
+          item = '00:'+item.trim(); 
+        }  
+        return item;
+      }).join(' --> ');
+      vttLine = EOL+vttLine+EOL
+    }
+    else if(line.match(reg))
+    {
+      var vttLine = line.replace(reg, '')
+    }
+    else
+    {
+      var vttLine = line+EOL;
+    }     
+                  
+
+                  
+
+
+    if (!vttLine.trim()) return cb();
+
+        if (/^Kind:|^Language:/m.test(vttLine)) {
+            return cb();
+          }
+      
+         if (/^[0-9]+:/m.test(vttLine)) {
+            if (count === 0) {
+              vttLine = ++count + EOL + vttLine;
+            } else {
+              vttLine = EOL + ++count + EOL + vttLine;
+            }
+          }
+
+    cb(null, vttLine);
+  };
+
+  return (0, _pumpify2.default)((0, _split2.default)(), _through2.default.obj(write));
+};
+
+/*
+import { EOL } from 'os';
+
     let count = 0;
     const reg = new RegExp(`(WEBVTT\s*(FILE)?.*)(${EOL})*`, 'g');
+
     const write = (line, enc, cb) => {
 
         if (!line.trim()) return cb();
@@ -29,15 +109,16 @@ module.exports = function () {
             }).join(' --> ');
             vttLine = EOL + vttLine + EOL;
         }
-        else if (line.match(reg)
-        {
+        else if (line.match(reg) {
             let vttLine = line.replace(reg, '');
         }
         else {
             let vttLine = line + EOL;
         }
 
-        if (!vttLine.trim()) return cb();
+        if (!vttLine.trim()) {
+            return cb()
+        };
 
         if (/^Kind:|^Language:/m.test(vttLine)) {
             return cb();
@@ -55,6 +136,20 @@ module.exports = function () {
 
     };
 
+module.exports = function () {
+
+
     return pumpify(split(), through.obj(write));
 
 };
+*/
+
+
+
+
+
+
+
+
+
+
