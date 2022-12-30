5161125a76e1a4b090adeba6f2350d46a023441f
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-30 15:35:36 -0800

    WIP. utils-vtt. code clean up

--- src/utils/utils-vtt.ts
+++ src/utils/utils-vtt.ts
@@ -1,6 +1,6 @@
 import { EOL } from 'os';
 
-export function convertVttToSrt(fileContent: string) {
+export function convertVttToSrt(fileContent: string): string {
 
     let ccCount = 0;
     const regFirstLine = new RegExp(`(WEBVTT\s*(FILE)?.*)(${EOL})*`, 'g');
@@ -61,69 +61,3 @@ export function convertVttToSrt(fileContent: string) {
     const lines = fileContent.split(/\r?\n/);
     return lines.map(convertLine).filter((line) => line !== undefined).join('');
 }
-
-/*
-import { EOL } from 'os';
-
-    let count = 0;
-    const reg = new RegExp(`(WEBVTT\s*(FILE)?.*)(${EOL})*`, 'g');
-
-    const write = (line, enc, cb) => {
-
-        if (!line.trim()) return cb();
-        if (line.match(/(\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2})\.(\d{3}\s*)/g)) {
-            let vttComp = line.split('-->');
-            let vttLine = vttComp.map(function (item) {
-                item = item.replace('.', ',');
-                if (item.split(":").length < 3) {
-                    item = '00:' + item.trim();
-                }
-                return item;
-            }).join(' --> ');
-            vttLine = vttLine + EOL;
-        }
-        else if (line.match(/(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)/g)) {
-            let vttComp = line.split('-->');
-            let vttLine = vttComp.map(function (item) {
-                item = item.replace('.', ',');
-                if (item.split(":").length < 3) {
-                    item = '00:' + item.trim();
-                }
-                return item;
-            }).join(' --> ');
-            vttLine = EOL + vttLine + EOL;
-        }
-        else if (line.match(reg) {
-            let vttLine = line.replace(reg, '');
-        }
-        else {
-            let vttLine = line + EOL;
-        }
-
-        if (!vttLine.trim()) {
-            return cb()
-        };
-
-        if (/^Kind:|^Language:/m.test(vttLine)) {
-            return cb();
-        }
-
-        if (/^[0-9]+:/m.test(vttLine)) {
-            if (count === 0) {
-                vttLine = `${++count}${EOL}${vttLine}`;
-            } else {
-                vttLine = `${EOL}${++count}${EOL}{vttLine}`;
-            }
-        }
-
-        cb(null, vttLine);
-
-    };
-
-module.exports = function () {
-
-
-    return pumpify(split(), through.obj(write));
-
-};
-*/
